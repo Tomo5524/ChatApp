2542c4b92c46860cd7b7bb5286f2c653fcd98eeb
Tomo5524 <ttoommoosrst@gmail.com>
2021-03-17 13:34:24 +0900

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+// import Login from "./components/login";
+import Signup from "./components/sign-up";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <Signup />
+    </div>
+  );
+}
+
+export default App;
