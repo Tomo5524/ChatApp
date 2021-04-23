bd5269c32a8eb48052e8e41c691ba67caa152815
Tomo5524 <ttoommoosrst@gmail.com>
2021-04-23 21:54:13 +0900

    daily commit

--- client/src/App.js
+++ client/src/App.js
@@ -16,25 +16,17 @@ function App() {
   return (
     <div>
       {/* <h1>Hello World</h1> */}
-      {/* <BrowserRouter basename={process.env.PUBLIC_URL}> */}
       <Router>
         <div>
-          {/* <NavBar /> */}
-          {/* <Signup /> */}
+          {/* makes user a global variable */}
           <UserContext.Provider value={{ user, setUser }}>
             <Switch>
-              {/* <Route exact path="/" component={<Home currentUser={currentUser} />} /> */}
               <Route exact path="/" component={Home} />
               <Route path="/sign-up" component={Signup} />
               <Route path="/login" component={Login} />
               <Route path="/chat/:userSlug/:roomSlug" component={Chat} />
               <Route path="/chat/:userSlug" component={Room} />
               <Route path="/create-room" component={AddRoom} />
-
-              {/* looks like route has some order */}
-              {/* <Route path="/chat/:userSlug/:roomSlug" component={Chat} /> */}
-
-              {/* <Route path="/sign-up" component={Signup} /> for some reason, if signup component is here, showPost gets called first and causes an error */}
             </Switch>
           </UserContext.Provider>
         </div>
--- client/src/UserContext.js
+++ client/src/UserContext.js
@@ -1,4 +1,3 @@
 import { createContext } from "react";
-// import { authHeader, logOut, getUser } from "./services/auth";
 
 export const UserContext = createContext();
